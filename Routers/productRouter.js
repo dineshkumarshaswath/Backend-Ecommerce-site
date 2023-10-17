5292ea4ab43389b5a325b3290985da004ee26a8c
dineshkumarshaswath <dineshshaswath@gmail.com>
2023-10-17 10:30:25 +0530

    adminproducts

--- Routers/productRouter.js
+++ Routers/productRouter.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const { getProduct, newProduct, getSingleProduct, 
-    updateProduct, deleteProduct, createReview, getReviews, deleteReview } = require("../Controllers/productControllers")
+    updateProduct, deleteProduct, createReview, getReviews, deleteReview, getAdminProducts } = require("../Controllers/productControllers")
 const { isAuthenticatedUser,isAuthorizeRoles } = require("../middleware/authenticateuser")
 
 
@@ -8,6 +8,8 @@ const router=express.Router()
 
 router.route("/products").get(getProduct)
 router.route("/products/new").post(isAuthenticatedUser,isAuthorizeRoles('admin'),newProduct)
+router.route("/admin/products").post(isAuthenticatedUser,isAuthorizeRoles('admin'),getAdminProducts)
+
 router.route("/products/:id").get(getSingleProduct).put(updateProduct)
                               .delete(deleteProduct);
 router.route("/allreviews").get(getReviews)
