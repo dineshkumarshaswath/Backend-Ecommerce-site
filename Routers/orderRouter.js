4056b4a40101ad8a544bef3dada9bee13ec5a5f1
dineshkumarshaswath <dineshshaswath@gmail.com>
2023-10-20 14:32:06 +0530

    orderupdate

--- Routers/orderRouter.js
+++ Routers/orderRouter.js
@@ -9,8 +9,8 @@ router.route("/order/:id").get(isAuthenticatedUser,getSingleOrder)
 router.route("/myorders").get(isAuthenticatedUser,myOrders)
 
 //Admin order routes
-router.route("/allorders").get(isAuthenticatedUser,isAuthorizeRoles('admin'),getAllOrders)
-router.route("/order/:id").put(isAuthenticatedUser,isAuthorizeRoles('admin'),updateOrder)
+router.route("/admin/orders").get(isAuthenticatedUser,isAuthorizeRoles('admin'),getAllOrders)
+router.route("/admin/order/:id").put(isAuthenticatedUser,isAuthorizeRoles('admin'),updateOrder)
                            .delete(isAuthenticatedUser,isAuthorizeRoles('admin'),deleteOrder)
 
 module.exports=router
